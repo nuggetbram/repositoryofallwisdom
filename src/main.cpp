964106adf186bc052441af74a1466fc3d9f9e4d9
aetherum <admin@aetherum.com>
2015-02-21 20:10:50 +1100

    stuuf

--- src/main.cpp
+++ src/main.cpp
@@ -1500,11 +1500,12 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
 
     // Check range
     if (bnTarget <= 0 || bnTarget > bnProofOfWorkLimit)
-        return error("CheckProofOfWork() : nBits below minimum work");
-
+        //return error("CheckProofOfWork() : nBits below minimum work");
+	return false;
     // Check proof of work matches claimed amount
     if (hash > bnTarget.getuint256())
-        return error("CheckProofOfWork() : hash doesn't match nBits");
+        //return error("CheckProofOfWork() : hash doesn't match nBits");
+	return false;
 
     return true;
 }
